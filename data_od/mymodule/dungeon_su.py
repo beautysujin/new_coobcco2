966d47a7540dbdc45ab17a60d85e8c7b71effc91
beautysujin <beautysujin7@gmail.com>
2023-06-29 02:20:31 +0900

    ver 1.111

--- data_od/mymodule/dungeon_su.py
+++ data_od/mymodule/dungeon_su.py
@@ -123,7 +123,7 @@ def dun_go(cla, dun_where, dun_step):
             full_path = "c:\\my_games\\coobcco2\\data_od\\imgs\\dunjeon\\dungeon_title_4_shadow.PNG"
             img_array = np.fromfile(full_path, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-            imgs_ = imgs_set_(20, 350, 920, 410, cla, img, 0.88)
+            imgs_ = imgs_set_(20, 350, 920, 410, cla, img, 0.9)
             # 드래그
             if imgs_ is not None and imgs_ != False:
                 drag_pos(850, 500, 130, 500, cla)
